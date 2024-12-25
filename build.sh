a1afee948847f2c8df0bc9d093d046d22ff6a3d0
amgs <user@example.com>
2024-12-25 13:53:33 +0000

    Enable export.

---
+++ build.sh
@@ -0,0 +1,7 @@
+cd 01_sociedade_em_rede_e_educacao_digital 
+pandoc 01_00_sociedade_em_rede_e_educacao_digital.md -o ../output/01_00_sociedade_em_rede_e_educacao_digital.pdf -H ../preamble.tex
+pandoc 01_01_sociedade_em_rede_e_os_novos_desafios_da_educacao.md -o ../output/01_01_sociedade_em_rede_e_os_novos_desafios_da_educacao.pdf -H ../preamble.tex
+pandoc 01_02_a_realidade_hiperconectada_e_a_educacao_online.md -o ../output/01_02_a_realidade_hiperconectada_e_a_educacao_online.pdf -H ../preamble.tex
+pandoc 01_03_o_paradigma_da_educomunicacao.md -o ../output/01_03_o_paradigma_da_educomunicacao.pdf -H ../preamble.tex
+pandoc 01_04_a_inovacao_em_educacao_digital.md -o ../output/01_04_a_inovacao_em_educacao_digital.pdf -H ../preamble.tex
+cd ..
